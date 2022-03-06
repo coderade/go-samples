e3b0cb7f88c98dd470fbc25489088c3321820de6
Valdeci Gomes <valfilho@amazon.com>
2022-03-05 21:38:53 -0800

    Update maps example, add delete

--- maps/main.go
+++ maps/main.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
+
+	// Different ways of creating a map
+	colors1 := map[string]string{
+		"red":   "#FF0000",
 		"green": "#008000",
 	}
-	fmt.Println(colors)
+	fmt.Println(colors1)
+
+	//var colors2 map[string]string
+	//colors2["blue"] = "#3389FF"
+	//fmt.Println(colors2)
+
+	//colors3 := make(map[string]string)
+	//colors3["white"] = "#FFFFFF"
+	//fmt.Println(colors3)
+
+	delete(colors1, "red")
+	fmt.Println(colors1)
 }
