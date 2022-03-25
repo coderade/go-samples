30031e13f51ab2f62e96a12c8471df45dcfc857c
Valdeci Gomes <valfilho@amazon.com>
2022-03-25 18:31:08 -0300

    Add synchronous get request example

---
+++ channels/sync_example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	urls := []string{
+		"https://google.com",
+		"https://amazon.com",
+		"https://golang.org",
+	}
+
+	for _, url := range urls {
+		go checkSite(url)
+	}
+}
+
+func checkSite(url string) {
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println(url, "might be down")
+	}
+	fmt.Println(url, "is up!")
+}
