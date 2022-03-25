fbb7a3d559648237b07bfb6555a35680ab7524cb
Valdeci Gomes <valfilho@amazon.com>
2022-03-25 09:02:25 -0300

    Add Custom writer interface example

--- interfaces/http/main.go
+++ interfaces/http/main.go
@@ -20,7 +20,20 @@ func main() {
 	//response.Body.Read(bs)
 	//fmt.Println(string(bs))
 
-	//Using the io.Copy from os.Stdout to print the response.body
-	io.Copy(os.Stdout, response.Body)
+	// Using the io.Copy from os.Stdout to print the response.body
+	//io.Copy(os.Stdout, response.Body)
 
+	// custom Log writer
+	lw := LogWriter{}
+	io.Copy(lw, response.Body)
+}
+
+type LogWriter struct{}
+
+func (LogWriter) Write(bs []byte) (int, error) {
+	bytesSize := len(bs)
+	fmt.Println(string(bs))
+	fmt.Println("bytes size: ", bytesSize)
+
+	return bytesSize, nil
 }
